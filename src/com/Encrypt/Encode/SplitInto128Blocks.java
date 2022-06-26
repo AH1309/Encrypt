2d23e0faec003bc75cc8de61c4376e8638a39d28
AH1309 <79544568+AH1309@users.noreply.github.com>
2022-06-26 20:58:57 +0100

    Arraylist implemented

--- src/com/Encrypt/Encode/SplitInto128Blocks.java
+++ src/com/Encrypt/Encode/SplitInto128Blocks.java
@@ -7,27 +7,26 @@ import java.util.ArrayList;
 
 
 public class SplitInto128Blocks {
-    public static byte[][] Split128(byte [] args){
-        byte[][] returnvariable = null; //!!!! NEEDS TO BE FIXED
+    public static ArrayList<ArrayList<Byte>> Split128(ArrayList<Byte> args){
+
         if (args == null){
             return null;
 
         }
+
         ArrayList<ArrayList<Byte>> returnlist = new ArrayList<ArrayList<Byte>>();
 
 
-        for(int i = 1; i <= (int) Math.ceil((float) args.length / 128) ; i++){
+        for(int i = 1; i <= (int) Math.ceil((float) args.size() / 128) ; i++){
 
-            ByteBuffer buffer = ByteBuffer.wrap(args);
-            byte[] splited = new byte[128];
-            buffer.get(splited);
+          //  ByteBuffer buffer = ByteBuffer.wrap(args); // Legacy method
+           // byte[] splited = new byte[128];
+          //  buffer.get(splited);
 
 
         }
-        return returnvariable; // NEEDS TO BE FIXED
-
+        return null;
     }
-    
         public static void lengthequalsmod128(String[] args) {
         ArrayList<Byte> data = new ArrayList<Byte>();
         data.add((byte) 12);
