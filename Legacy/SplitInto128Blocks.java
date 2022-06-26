88160f88c86996dbe2ffd26e0e9ed97d733e7680
AH1309 <79544568+AH1309@users.noreply.github.com>
2022-06-26 20:57:01 +0100

    Create SplitInto128Blocks.java

---
+++ Legacy/SplitInto128Blocks.java
@@ -0,0 +1,21 @@
+public static byte[][] Split128(byte [] args){
+        byte[][] returnvariable = null; //!!!! NEEDS TO BE FIXED
+        if (args == null){
+            return null;
+
+        }
+        ArrayList<ArrayList<Byte>> returnlist = new ArrayList<ArrayList<Byte>>();
+
+
+        for(int i = 1; i <= (int) Math.ceil((float) args.length / 128) ; i++){
+
+            ByteBuffer buffer = ByteBuffer.wrap(args);
+            byte[] splited = new byte[128];
+            buffer.get(splited);
+
+
+        }
+        return returnvariable; // NEEDS TO BE FIXED
+
+    }
+
