de26acd695c0c321da4d838e90c9c22f43d39f35
AH1309 <79544568+AH1309@users.noreply.github.com>
2022-06-26 19:29:57 +0100

    needs to be fixed
     byte[][] returnvariable = null; //!!!! NEEDS TO BE FIXED

--- src/com/Encrypt/Encode/SplitInto128Blocks.java
+++ src/com/Encrypt/Encode/SplitInto128Blocks.java
@@ -1,8 +1,30 @@
 package com.Encrypt.Encode;
 
-public class SplitInto128Blocks {
-    public static byte[][] Split128(byte[] args){
+import java.lang.Byte;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
 
+
+
+public class SplitInto128Blocks {
+    public static byte[][] Split128(byte [] args){
+        byte[][] returnvariable = null; //!!!! NEEDS TO BE FIXED
+        if (args == null){
+            return null;
+
+        }
+        ArrayList<ArrayList<Byte>> returnlist = new ArrayList<ArrayList<Byte>>();
+
+
+        for(int i = 1; i <= (int) Math.ceil((float) args.length / 128) ; i++){
+
+            ByteBuffer buffer = ByteBuffer.wrap(args);
+            byte[] splited = new byte[128];
+            buffer.get(splited);
+
+
+        }
+        return returnvariable; // NEEDS TO BE FIXED
 
     }
 }
