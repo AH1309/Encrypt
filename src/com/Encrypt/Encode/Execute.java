7c2ae5ab948202dcde2a9107d83839738db5f016
AH1309 <79544568+AH1309@users.noreply.github.com>
2022-06-30 17:22:06 +0100

    updated
    not finished

--- src/com/Encrypt/Encode/Execute.java
+++ src/com/Encrypt/Encode/Execute.java
@@ -1,10 +1,15 @@
 package com.Encrypt.Encode;
 
+
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
 import java.nio.charset.CharacterCodingException;
 import java.nio.charset.Charset;
 import java.nio.charset.CodingErrorAction;
+import java.util.ArrayList;
+
+
+
 
 public class Execute {
     public static byte[] strictStringToBytes(String s, Charset charset) throws CharacterCodingException {
@@ -15,11 +20,42 @@ public class Execute {
     }
     public static String Encrypt(String key , String data) throws CharacterCodingException{
         byte[] datatobyte = strictStringToBytes(data, Charset.forName("US-ASCII"));
-        byte[][] encryptingthing = SplitInto128Blocks.Split128(datatobyte);
+        ArrayList<Byte> nearlyfinished = ArraytoArraylist(datatobyte);
+        ArrayList<ArrayList<Byte>> finished = SplitInto128Blocks.Split128(nearlyfinished);
         
         
+        // tablehash function and replacement
+
+        // right rotates
+
+        // deconversion
+
+
         
-        
-        return encryptedthing; 
+
+
+
+
+
+        return encryptedthing;
+    }
+
+    public static byte[] ArraylistToarray(ArrayList<Byte> args)  {
+        byte[] returnlist = new byte[ args.size()];
+        for (int i = 0 ; i < args.size(); i++) {
+            returnlist[i] = args.get(i);
+
+
+        }
+        return returnlist;
+    }
+    public static ArrayList<Byte> ArraytoArraylist(byte[] args)  {
+        ArrayList<Byte> returnlist = new ArrayList<Byte>();
+        for (int i = 0 ; i < args.length ; i++) {
+            returnlist.add(args[i]);
+
+
+        }
+        return returnlist;
     }
 }
